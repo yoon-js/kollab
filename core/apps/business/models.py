2014ca69444f47b571cb3b9674950879b3570647
Tony Lee <43350428+TonysHub@users.noreply.github.com>
2023-05-11 21:12:02 +0900

    define business models

--- core/apps/business/models.py
+++ core/apps/business/models.py
@@ -1,3 +1,40 @@
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+class KollabBusiness(models.Model):
+    id = models.AutoField(primary_key=True)
+    created = models.DateTimeField(auto_now_add=True)
+    deleted = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    logo = models.ImageField(upload_to='business/logos')
+    cover = models.ImageField(upload_to='business/covers')
+    website = models.URLField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    is_active = models.BooleanField(default=True)
+    campaign_id = models.CharField(max_length=100)
+    tier_id = models.CharField(max_length=100)
+    detail_id = models.CharField(max_length=100)
+
+class KollabCampaign(models.Model):
+    id = models.AutoField(primary_key=True)
+    created = models.DateTimeField(auto_now_add=True)
+    deleted = models.DateTimeField(auto_now=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    bid = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    type = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='business/campaigns')
+    business_id = models.CharField(max_length=100)
+    creator_id = models.CharField(max_length=100)
+    currency_id = models.CharField(max_length=100)
+
+class KollabTier(models.Model):
+    id = models.AutoField(primary_key=True)
+    tier = models.CharField(max_length=100)
